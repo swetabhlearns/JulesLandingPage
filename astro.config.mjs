166958c4ae5124dd6dc9665ffc4e3796f511f7d5
Swetabh Kumar <swetabh.learn@gmail.com>
2026-04-16 22:02:54 +0530

    Improve SEO and responsive media handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://bkttactical.com',
   output: 'static',
   integrations: [tailwind()],
 });
